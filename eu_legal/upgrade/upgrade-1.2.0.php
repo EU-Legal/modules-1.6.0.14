1c372d86cfdee2abd6fb36ffaa63d08748014093
Chris Gurk <t_github@gurkcity.de>
2015-02-24 23:09:42 +0100

    upgrade 1.2.0 fixes / adaptions

--- eu_legal/upgrade/upgrade-1.2.0.php
+++ eu_legal/upgrade/upgrade-1.2.0.php
@@ -4,10 +4,18 @@ function upgrade_module_1_2_0($eu_legal) {
 	
 	$result = true;
 	
-	if(is_file(_PS_MODULE_DIR_.$eu_legal->name.'/modules/paypal.zip'))
-		$result &= unlink(_PS_MODULE_DIR_.$eu_legal->name.'/modules/paypal.zip');
+	if(is_file(_PS_MODULE_DIR_.$eu_legal->name.'/modules/cashondelivery.zip'))
+		$result &= unlink(_PS_MODULE_DIR_.$eu_legal->name.'/modules/cashondelivery.zip');
 	if(is_file(_PS_MODULE_DIR_.$eu_legal->name.'/modules/gc_ganalytics.zip'))
 		$result &= unlink(_PS_MODULE_DIR_.$eu_legal->name.'/modules/gc_ganalytics.zip');
+	if(is_file(_PS_MODULE_DIR_.$eu_legal->name.'/modules/moneybookers.zip'))
+		$result &= unlink(_PS_MODULE_DIR_.$eu_legal->name.'/modules/moneybookers.zip');
+	if(is_file(_PS_MODULE_DIR_.$eu_legal->name.'/modules/paypal.zip'))
+		$result &= unlink(_PS_MODULE_DIR_.$eu_legal->name.'/modules/paypal.zip');
+	if(is_file(_PS_MODULE_DIR_.$eu_legal->name.'/modules/sofortbanking.zip'))
+		$result &= unlink(_PS_MODULE_DIR_.$eu_legal->name.'/modules/sofortbanking.zip');
+	if(is_file(_PS_MODULE_DIR_.$eu_legal->name.'/modules/trustedshops.zip'))
+		$result &= unlink(_PS_MODULE_DIR_.$eu_legal->name.'/modules/trustedshops.zip');
 	
 	Autoload::getInstance()->generateIndex();
 	
