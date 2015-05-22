8a535745ac8a9d231eb9616f2a5e63a5c3a52125
Chris Gurk <t_github@gurkcity.de>
2015-05-22 15:02:59 +0200

    new module version 1.4.2

--- eu_legal/eu_legal.php
+++ eu_legal/eu_legal.php
@@ -2,8 +2,8 @@
 /**
  * EU Legal - Better security for German and EU merchants.
  *
- * @version   : 1.4.1
- * @date      : 2015 05 07
+ * @version   : 1.4.2
+ * @date      : 2015 05 22
  * @author    : Markus Engel/Chris Gurk @ Onlineshop-Module.de | George June/Alexey Dermenzhy @ Silbersaiten.de
  * @copyright : 2015 Onlineshop-Module.de | 2015 Silbersaiten.de
  * @contact   : info@onlineshop-module.de | info@silbersaiten.de
@@ -56,7 +56,7 @@ class EU_Legal extends Module
 		$this->tab = 'administration';
 
 		// version: major, minor, bugfix
-		$this->version = '1.4.1';
+		$this->version = '1.4.2';
 
 		// author
 		$this->author = 'EU Legal Team';
